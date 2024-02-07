bf170f09b84d8a42615dc6ba5a8f2bf394acacfb
danys <dany.sery@seznam.cz>
2024-02-07 20:40:50 +0100

    Removed wrong copyrights

--- BattleChess3.Core/Model/Figures/FigureActionTypes.cs
+++ BattleChess3.Core/Model/Figures/FigureActionTypes.cs
@@ -1,12 +1,4 @@
-﻿#region Copyright FEI Company 2024
-
-// All rights are reserved. Reproduction or transmission in whole or in part, in
-// any form or by any means, electronic, mechanical or otherwise, is prohibited
-// without the prior written consent of the copyright owner.
-
-#endregion
-
-namespace BattleChess3.Core.Model.Figures;
+﻿namespace BattleChess3.Core.Model.Figures;
 
 /// <summary>
 ///     Get possible figure actions.
