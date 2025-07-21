31bf93a731b245a99f17813741a4e2c5fc2863d8
danys <dany.sery@seznam.cz>
2025-07-21 23:15:49 +0200

    Fixed window scaling

---
+++ BattleChess3.UI/Converters/BoardSizeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace BattleChess3.UI.Converters;
+
+public class BoardSizeConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values is [double width, double height])
+        {
+            if (height >= width - 300)
+            {
+                return width - 300;
+            }
+
+            if (height <= width)
+            {
+                return height;
+            }
+            
+            return width;
+        }
+        return 0;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
