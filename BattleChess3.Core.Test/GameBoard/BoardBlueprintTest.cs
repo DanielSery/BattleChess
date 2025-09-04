3db7e429a57f4410a2da893c8bd79237b1a82358
danys <dany.sery@seznam.cz>
2025-09-04 20:53:14 +0200

    Added verification of chess board

---
+++ BattleChess3.Core.Test/GameBoard/BoardBlueprintTest.cs
@@ -0,0 +1,12 @@
+﻿using BattleChess3.Core.GameBoard;
+
+namespace BattleChess3.Core.Test.GameBoard;
+
+public class BoardBlueprintTest
+{
+    [Fact]
+    public Task VerifyChessBoard()
+    {
+        return Verify(BoardBlueprint.ChessTeam);
+    }
+}
