4635fbab6db269a0858b4edb8eca838d50343f7d
Daniel Sery <daniel.sery@veeam.com>
2025-09-10 16:18:39 +0200

    added aggresive inlining for position calculation

--- CrownsGuard.Core/Helpers/ArrayPoolHelper.cs
+++ CrownsGuard.Core/Helpers/ArrayPoolHelper.cs
@@ -1,17 +1,20 @@
 // Copyright (c) Veeam Software Group GmbH
 
 using System.Buffers;
+using System.Runtime.CompilerServices;
 
 namespace CrownsGuard.Core.Helpers;
 
 public static class ArrayPoolHelper
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ArrayPoolMemory<T> Rent<T>(int count)
     {
         var array = ArrayPool<T>.Shared.Rent(count);
         return new ArrayPoolMemory<T>(array, count);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ArrayPoolMemory<T> ToArrayPoolMemory<T>(this Span<T> actions, int count)
     {
         var array = ArrayPool<T>.Shared.Rent(count);
