a3d3207d3d93fbb4b304a4fe5dab32f74848d7bf
danys <dany.sery@seznam.cz>
2025-09-11 22:56:14 +0200

    Further improvements

--- CrownsGuard.Core/Constants.cs
+++ CrownsGuard.Core/Constants.cs
@@ -4,9 +4,8 @@ public static class Constants
 {
     public const int KingValue = 1_000_000_000;
     
-    public const int FigureValueCoeff = 5000;
-    public const int UpgradeFigureImpact = 400;
-    
+    public const int FigureValueCoeff = 1_000;
+
     public const int RangedConvertCoeff = 600;
     public const int RangedAttackCoeff = 400;
     public const int HalfRangedAttackCoeff = 200;
@@ -17,7 +16,7 @@ public static class Constants
     public const int PossibleHalfRangedAttackImpact = 150;
     public const int PossibleMeeleeAttackImpact = 100;
 
-    public const int BuildImpact = 40;
+    public const int BuildImpact = 50;
     public const int MoveImpact = 10;
 
     public const int BoardLength = 8;
